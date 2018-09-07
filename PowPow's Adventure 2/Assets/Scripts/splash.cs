bbd2c933294dc9a2448097ec617827ed0fb90289
greggisgood <greggmeyrick@gmail.com>
2018-09-07 17:48:30 +0800

    Added basic splash screen and menu.

---
+++ PowPow's Adventure 2/Assets/Scripts/splash.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splash : MonoBehaviour {
+
+
+    public float delay;
+    public string newLevel = "Menu";
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(newLevel);
+    }
+}
