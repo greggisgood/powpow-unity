3d4da99bd1cb26a7ce0a7b84fe4bb39778733d04
greggisgood <greggmeyrick@gmail.com>
2018-09-12 12:01:55 +0800

    Displayed score on the upper left of the screen.

--- pow_adv0.1/Assets/Scripts/ScoreManager.cs
+++ pow_adv0.1/Assets/Scripts/ScoreManager.cs
@@ -6,32 +6,44 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour {
 
     public Text scoreText;
-    public Text highScoreText;
+   //public Text highScoreText;
 
     public float scoreCount;
     public float highScoreCount;
 
-    public float pointsPerSecond;
+    //public float pointsPerSecond;
 
     public bool scoreIncreasing;
-
+    int score;
 
 	// Use this for initialization
 	void Start () {
-		
+        score = 0;
+        InvokeRepeating("scoreUpdate", .01f, 0.1f);	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        scoreCount += pointsPerSecond * Time.deltaTime;
+        //scoreCount += pointsPerSecond * Time.deltaTime;
 
-        if (scoreCount > highScoreCount)
-        {
-            highScoreCount = scoreCount;
-        }
+        //if (scoreCount > highScoreCount)
+        //{
+        //    highScoreCount = scoreCount;
+        //}
 
-        scoreText.text = "Score: " + Mathf.Round(scoreCount);
-        highScoreText.text = "High Score: " + Mathf.Round(highScoreCount);
+        //scoreText.text = "Score: " + Mathf.Round(scoreCount);
+        //highScoreText.text = "High Score: " + Mathf.Round(highScoreCount);
+    }
+
+    void scoreUpdate()
+    {
+        score += 1;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void stopScoreUpdate()
+    {
+        CancelInvoke("scoreUpdate");
     }
 }
